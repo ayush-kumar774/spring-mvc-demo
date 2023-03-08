79307e8cf01962c77f5346c92e5d654534345b4e
Aayush Kumar <53551688+ayush-kumar774@users.noreply.github.com>
2023-03-08 10:15:19 +0530

    Added RequestParam

--- src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -16,12 +17,20 @@ public class HelloWorldController {
 		return "helloworld-form";
 	}
 	
+	// need a controller method to show the newer version of form
+	@RequestMapping("/showFormV2") 
+	public String showFormV2() {
+		return "helloworld-form-version2";
+	}
+	
 	// need a controller method to process the HTML form
 	@RequestMapping("/processForm") 
 	public String processForm() {
 		return "helloworld";
 	}
 	
+	
+	// need a controller method to read form data and add data to the model
 	@RequestMapping("/processFormVersionTwo")
 	public String letsShoutDude(HttpServletRequest request, Model model) {
 		
@@ -32,10 +41,27 @@ public class HelloWorldController {
 		theName = theName.toUpperCase();
 		
 		// create the message
-		String result = "Yo !! " + theName ;
+		String result = "Yo Yo !! " + theName ;
 		
 		// add message to the model 
-		model.addAttribute("message", result);
+		model.addAttribute("message", result); // first parameter is name and second parameter is value
+		
+		return "helloworld" ;
+		
+	}
+	
+	// need a controller method to read form data and add data to the model
+	@RequestMapping("/processFormV3")
+	public String processFormUsingRequestParam(@RequestParam("studentName") String theName, Model model) {
+		
+		//convert the data to all caps
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hello from V3 !! " + theName ;
+		
+		// add message to the model 
+		model.addAttribute("message", result); // first parameter is name and second parameter is value
 		
 		return "helloworld" ;
 		
